7a34a5276ab1c597bcd1444315e5673153768f5e
apavlyuc <apavlyuc@gmail.com>
2019-04-29 00:07:43 +0300

    update read_rooms function|need tests

--- src/parser/reader.c
+++ src/parser/reader.c
@@ -14,25 +14,47 @@ static int		read_ants(t_farm *farm)
 	return (1);
 }
 
-static int		read_rooms(t_farm *farm)
+static int		read_rooms(t_farm *farm, char **line)
 {
-	(void)farm;
+	int			gnl_ret_code;
+	int			room_type;
+
+	room_type = 0;
+	while ((gnl_ret_code = get_next_line(0, line)))
+	{
+		if (is_valid_room(*line))
+		{
+			add_room(&farm, get_room_name(*line), room_type);
+		}
+		else if (is_valid_comment(*line))
+		{
+			room_type = get_next_room_type(*line);
+		}
+		else
+			break;
+		ft_memdel((void **)line);
+	}
+	if (gnl_ret_code == -1)
+		return (0);
 	return (1);
 }
 
-static int		read_links(t_farm *farm)
+static int		read_links(t_farm *farm, char **first_link)
 {
 	(void)farm;
+	(void)first_link;
 	return (1);
 }
 
 int				read_farm(t_farm *farm)
 {
+	char		*first_link;
+
 	if (read_ants(farm) == 0)
 		return (0);
-	if (read_rooms(farm) == 0)
+	if (read_rooms(farm, &first_link) == 0)
 		return (0);
-	if (read_links(farm) == 0)
+	if (read_links(farm, &first_link) == 0)
 		return (0);
 	return (1);
 }
