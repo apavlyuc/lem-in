70c535cf1d7f333eda128988313926b412376e46
apavlyuc <apavlyuc@gmail.com>
2019-04-29 00:06:37 +0300

    add empty function body for each function from "validator" block

--- src/parser/validator.c
+++ src/parser/validator.c
@@ -1,5 +1,23 @@
 #include "../../inc/lemin.h"
 
+int				is_valid_room(char *room)
+{
+	(void)room;
+	return (1);
+}
+
+int				is_valid_comment(char *comment)
+{
+	(void)comment;
+	return (1);
+}
+
+int				is_valid_link(char *link)
+{
+	(void)link;
+	return (1);
+}
+
 int				validate_farm(t_farm *farm)
 {
 	(void)farm;
