b003f8d0634c1fb170b2da2c5315149ecbeea8ae
apavlyuc <apavlyuc@gmail.com>
2019-04-29 00:24:42 +0300

    update is_valid_room function|need tests

--- src/parser/validator.c
+++ src/parser/validator.c
@@ -1,8 +1,21 @@
+#include "../../libft/inc/libft.h"
 #include "../../inc/lemin.h"
 
 int				is_valid_room(char *room)
 {
-	(void)room;
+	t_ull		size;
+	char		*x_entry_point;
+	char		*y_entry_point;
+
+	size = ft_strlen(room);
+	if (size == 0 || *room == '#' || *room == 'L')
+		return (0);
+	x_entry_point = ft_strchr(room, ' ');
+	if (!x_entry_point || !ft_isdigit(*(++x_entry_point)))
+		return (0);
+	y_entry_point = ft_strchr(x_entry_point, ' ');
+	if (!y_entry_point || !ft_isdigit(*(++y_entry_point)))
+		return (0);
 	return (1);
 }
 
