36057a3b92a9c638deb597bd7f666261ddce4d6c
apavlyuc <apavlyuc@gmail.com>
2019-04-29 00:47:59 +0300

    update get_room_name function | need tests

--- src/parser/getters.c
+++ src/parser/getters.c
@@ -1,3 +1,4 @@
+#include "../../libft/inc/libft.h"
 
 int				get_next_room_type(char *line)
 {
@@ -7,6 +8,10 @@ int				get_next_room_type(char *line)
 
 char			*get_room_name(char *line)
 {
-	(void)line;
-	return (0);
+	char		*name;
+	char		*first_space_entry;
+
+	first_space_entry = ft_strchr(line, ' ');
+	name = ft_strsub(line, 0, first_space_entry - line);
+	return (name);
 }
