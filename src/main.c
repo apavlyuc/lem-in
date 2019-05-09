c795ca32199c6bcc3d3ef7d5dcd4dd516634a8e4
apavlyuc <apavlyuc@gmail.com>
2019-05-09 17:43:34 +0300

    add debug info + printing functions in main.c | TEMP

--- src/main.c
+++ src/main.c
@@ -3,7 +3,37 @@
 
 #include <unistd.h>
 
-static void	set_to_zero_state(t_farm *farm)
+void			print_all(t_farm *farm)
+{
+	printf("ants: %d\n", farm->ants_count);
+	printf("start_room: %s\n", farm->start_room_name);
+	printf("finish_room: %s\n", farm->finish_room_name);
+	printf("\nrooms:\n");
+	t_list *tmp_room = farm->rooms;
+	while (tmp_room)
+	{
+		printf("%s\t", (char *)tmp_room->content);
+		tmp_room = tmp_room->next;
+	}
+	printf("\n\nlinks:\n");
+	t_list *tmp_link = farm->links;
+	while (tmp_link)
+	{
+		t_binding *tmp = (t_binding *)tmp_link->content;
+		printf("bind: %s -> [", tmp->node);
+		t_list *tmp_neighbour = tmp->neighbours;
+		while (tmp_neighbour)
+		{
+			printf(" %s ", (char *)tmp_neighbour->content);
+			tmp_neighbour = tmp_neighbour->next;
+		}
+		printf("]\n");
+		tmp_link = tmp_link->next;
+	}
+	printf("nice :3\n");
+}
+
+static void		set_to_zero_state(t_farm *farm)
 {
 	farm->ants_count = 0;
 	farm->links = 0;
@@ -12,27 +42,49 @@ static void	set_to_zero_state(t_farm *farm)
 	farm->finish_room_name = 0;
 }
 
-static void	delete_farm_fields(t_farm *farm)
+static void		delete_link_binding(t_list *link)
 {
+	t_binding	*binding;
+
+	if (!link || !link->content)
+		return;
+	binding = (t_binding *)link->content;
+	ft_memdel((void **)&(binding->node));
+	ft_lstdel(&(binding->neighbours), ft_del_handler);
+}
+
+static void		delete_farm_fields(t_farm *farm)
+{
+	t_list		*link_to_clean;
+
 	ft_memdel((void **)&farm->start_room_name);
 	ft_memdel((void **)&farm->finish_room_name);
 	ft_lstdel(&(farm->rooms), ft_del_handler);
+	link_to_clean = farm->links;
+	while (link_to_clean)
+	{
+		delete_link_binding(link_to_clean);
+		link_to_clean = link_to_clean->next;
+	}
 	ft_lstdel(&(farm->links), ft_del_handler);
 }
 
-int			main(int ac, char **av)
+int				main(int ac, char **av)
 {
-	t_farm	game_info;
+	t_farm		game_info;
+	int			a, b;
 
 	(void)ac;
 	(void)av;
 	set_to_zero_state(&game_info);
-	if (read_farm(&game_info) == 0 || validate_farm(&game_info) == 0)
+	if ((a = read_farm(&game_info)) == 0 || (b = validate_farm(&game_info) == 0))
 	{
 		delete_farm_fields(&game_info);
+		printf("ivalid: [a = %d | b = %d]\n", a, b);
 		return (-1);
 	}
-
+	ft_putendl("valid");
+	print_all(&game_info);
 	delete_farm_fields(&game_info);
 	return (0);
 }
