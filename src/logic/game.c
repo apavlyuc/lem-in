a8b53680f356fe3c7f9406789e590aafc8e105a5
Oleksandr Pavliuchuk <oleksandr.pavliuchuk@thomsonreuters.com>
2019-05-17 15:52:38 +0300

    add plan in comments

--- src/logic/game.c
+++ src/logic/game.c
@@ -1,6 +1,16 @@
 #include "../../libft/inc/libft.h"
 #include "../../inc/lemin.h"
 
+/*
+**		here would be smth like that
+**
+**		1) convert lists to arrays (optional)
+**		2) legacy "algorithm_big"
+**		3) cycle-part of legacy "main"
+**			3.1) legacy "find_ways" and "print_lem"
+**				OR
+**			3.2) legacy "print_lem"
+*/
 void	play_game(t_farm *farm)
 {
 	(void)farm;
