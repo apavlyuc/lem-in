fb0ee4c05f4e81938f77e0ed99cd9d63b60788bb
Alexander PAVLYUCHUK <apavlyuc@e1r5p13.unit.ua>
2019-05-25 14:25:55 +0300

    rm print_all func

--- src/main.c
+++ src/main.c
@@ -1,36 +1,6 @@
 #include "../libft/inc/libft.h"
 #include "../inc/lemin.h"
 
-void			print_all(t_farm *farm)
-{
-	printf("ants: %d\n", farm->ants_count);
-	printf("start_room: %s\n", farm->start_room_name);
-	printf("finish_room: %s\n", farm->finish_room_name);
-	printf("\nrooms:\n");
-	t_list *tmp_room = farm->rooms;
-	while (tmp_room)
-	{
-		printf("%s\t", (char *)tmp_room->content);
-		tmp_room = tmp_room->next;
-	}
-	printf("\n\nlinks:\n");
-	t_list *tmp_link = farm->links;
-	while (tmp_link)
-	{
-		t_binding *tmp = (t_binding *)tmp_link->content;
-		printf("bind: %s -> [", tmp->node);
-		t_list *tmp_neighbour = tmp->neighbours;
-		while (tmp_neighbour)
-		{
-			printf(" %s ", (char *)tmp_neighbour->content);
-			tmp_neighbour = tmp_neighbour->next;
-		}
-		printf("]\n");
-		tmp_link = tmp_link->next;
-	}
-	printf("nice :3\n");
-}
-
 static void		set_to_zero_state(t_farm *farm)
 {
 	farm->ants_count = 0;
